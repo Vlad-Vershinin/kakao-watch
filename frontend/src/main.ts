313fcad2695ac9b20e697d2fdf54dc1031dac219
Echo_1-1 <fya17102007@gmail.com>
2026-04-19 00:25:00 +0500

    Index prettified

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -19,6 +19,26 @@ function isValidJwt(token: string): boolean {
 var feedObject = document.getElementById('videoFeed');
 
 
+var linkContacts = document.getElementById('LinkContacts');
+if(linkContacts){
+    linkContacts.innerHTML = `<i data-lucide="book-user" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+
+var LinkAbout = document.getElementById('LinkAbout');
+if(LinkAbout){
+    LinkAbout.innerHTML = `<i data-lucide="info" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+var LinkTrends = document.getElementById('LinkTrends');
+if(LinkTrends){
+    LinkTrends.innerHTML = `<i data-lucide="chart-no-axes-column-decreasing" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+var LinkHome = document.getElementById('LinkHome');
+if(LinkHome){
+    LinkHome.innerHTML = `<i data-lucide="house" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+
+
+
 function showAuthState(): void {
     const container = document.getElementById('auth-buttons')!;
     const token = localStorage.getItem('token');
