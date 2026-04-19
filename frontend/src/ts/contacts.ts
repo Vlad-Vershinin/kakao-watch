72f923f3870e19562e8c416ce6fe33e825a0f0b2
Echo_1-1 <fya17102007@gmail.com>
2026-04-19 16:41:37 +0500

    Pages added
    Added two pages: About and Contact

---
+++ frontend/src/ts/contacts.ts
@@ -0,0 +1,22 @@
+import '../style.css';
+import { createIcons, icons } from 'lucide';
+
+createIcons({ icons });
+
+var linkContacts = document.getElementById('LinkContacts');
+if(linkContacts){
+    linkContacts.innerHTML = `<i data-lucide="book-user" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+
+var LinkAbout = document.getElementById('LinkAbout');
+if(LinkAbout){
+    LinkAbout.innerHTML = `<i data-lucide="info" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+var LinkTrends = document.getElementById('LinkTrends');
+if(LinkTrends){
+    LinkTrends.innerHTML = `<i data-lucide="chart-no-axes-column-decreasing" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
+var LinkHome = document.getElementById('LinkHome');
+if(LinkHome){
+    LinkHome.innerHTML = `<i data-lucide="house" class="w-5 h-5 text-text-tertiary justify-self-center"></i>`;
+}
