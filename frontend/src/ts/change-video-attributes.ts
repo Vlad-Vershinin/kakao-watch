d3fc8f7f080ea43b848db5f47172e23227178d59
Echo_1-1 <fya17102007@gmail.com>
2026-05-08 00:01:58 +0500

    Added a patch request for changing video attributes

--- frontend/src/ts/change-video-attributes.ts
+++ frontend/src/ts/change-video-attributes.ts
@@ -123,6 +123,71 @@ async function deleteVideo(id: string): Promise<boolean> {
     }
 }
 
+(document.getElementById('submitBtn') as HTMLButtonElement)!.addEventListener("click", updateVideoData);
+
+async function updateVideoData(){
+        const urlParams = new URLSearchParams(window.location.search);
+        const videoId = urlParams.get('id');
+        if (!videoId) return;
+
+        const submitBtn = document.getElementById('submitBtn') as HTMLButtonElement;
+        submitBtn.disabled = true;
+        let textBefore = submitBtn.textContent;
+        submitBtn.textContent = 'Загрузка...';
+    
+        const title = (document.getElementById('videoTitle') as HTMLTextAreaElement).value;
+        const description = (document.getElementById('videoDescription') as HTMLTextAreaElement).value;
+        //const access = document.getElementById('accessPanel')!.value;
+        
+        if (!title) {
+            alert('Заполните все обязательные поля');
+            submitBtn.disabled = false;
+            submitBtn.textContent = textBefore;
+            submitBtn.innerHTML = '<i data-lucide="upload" class="w-5 h-5"></i> Загрузить видео';
+            createIcons({ icons });
+            return;
+        }
+    
+        const formData = new FormData();
+        formData.append('Name', title);
+        formData.append('Description', description);
+    
+        const token = localStorage.getItem('token');
+    
+        try {
+            const response = await fetch(`/api/videos/${videoId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: formData,
+            });
+    
+            if (!response.ok) {
+                const error = await response.json().catch(() => ({ 
+                    message: `Ошибка при загрузке`
+                }));
+                
+                submitBtn.disabled = false;
+                submitBtn.textContent = textBefore;
+                alert(error.message || 'Ошибка при загрузке видео');
+                return;
+            }
+    
+            alert('Информация о видео успешно обновлена!');
+            history.back();
+        } catch (err) {
+            console.error('Ошибка сети:', err);
+            alert('Не удалось подключиться к серверу');
+            submitBtn.disabled = false;
+            submitBtn.innerHTML = '<i data-lucide="upload" class="w-5 h-5"></i> Загрузить видео';
+            createIcons({ icons });
+        }
+}
+
+
+
+
 createIcons({ icons });
 
 initPlayer();
